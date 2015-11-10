8c2ff38b348588266c0a13cddee29c0a99252f21
cleanforestco <cleanforestco@users.noreply.github.com>
2015-11-11 02:18:03 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,62 +1,74 @@
 <?php
-
-if( stristr( $_SERVER['SERVER_NAME'], "localhost" ) ) {
+if( stristr( $_SERVER['SERVER_NAME'], "domain.dev" ) ) {
 	/* DEVELOPMENT SETTINGS */
 	define('DB_NAME', 'name');
 	define('DB_USER', 'user');
 	define('DB_PASSWORD', 'password');
 	define('DB_HOST', 'localhost');
-	define('DB_CHARSET', 'utf8');
-	define('DB_COLLATE', '');
 	define('WP_DEBUG', true);
+	define('WP_DEBUG_DISPLAY', true);
+	define('WP_DEBUG_LOG', true);
+	define('WP_MEMORY_LIMIT', '99M');
+	define('WP_MAX_MEMORY_LIMIT', '256M' );
 	define('DISALLOW_FILE_EDIT', true); // DISABLE WP FILE EDITOR
-	define('DOMAIN_CURRENT_SITE', 'localhost');
 	define('SAVEQUERIES', true ); // USED WITH DEBUG BAR
 	define('WP_CACHE', true); // WP DEFAULT CACHING
-	define( 'LOCALINDICATOR_TEXT', 'DEVELOPMENT'); // USED BY LOCAL INDICATOR
+	define('WPLT_SERVER', 'DEVELOPMENT'); // USED BY WP LOCAL TOOLBOX
+	define('WPLT_COLOR', '#4267B1'); // USED BY WP LOCAL TOOLBOX
 	// NOTE: Jetpack will automatically enable development mode if
 	// there isn't a period in the hostname, i.e. localhost.
 	define( 'JETPACK_DEV_DEBUG', true); // JETPACK DEVELOPMENT MODE
-	define('ENVIRONMENT', 'DEVELOPMENT');
 } elseif ( stristr( $_SERVER['SERVER_NAME'], "staging.domain.com" ) ) {
 	/* STAGING SETTINGS */
 	define('DB_NAME', 'name');
 	define('DB_USER', 'user');
 	define('DB_PASSWORD', 'password');
 	define('DB_HOST', 'localhost');
-	define('DB_CHARSET', 'utf8');
-	define('DB_COLLATE', '');
-	define('WP_DEBUG', false);
+	define('WP_DEBUG', true);
+	define('WP_DEBUG_DISPLAY', false);
+	define('WP_DEBUG_LOG', true);
+	define('WP_MEMORY_LIMIT', '99M');
+	define('WP_MAX_MEMORY_LIMIT', '256M' );
 	define('DISALLOW_FILE_EDIT', true); // DISABLE WP FILE EDITOR
-	define('DOMAIN_CURRENT_SITE', 'staging.domain.com');
 	define('SAVEQUERIES', true ); // USED WITH DEBUG BAR
 	define('WP_CACHE', true); // WP DEFAULT CACHING
-	define( 'LOCALINDICATOR_TEXT', 'STAGING'); // USED BY LOCAL INDICATOR
+	define('WPLT_SERVER', 'STAGING'); // USED BY WP LOCAL TOOLBOX
+	//If not defined as PRODUCTION or LIVE, the plugin will enable
+	//Discourage search engines from indexing this site.
+	define('WPLT_COLOR', 'orange'); // USED BY WP LOCAL TOOLBOX
 	// NOTE: Jetpack will automatically enable development mode if
 	// there isn't a period in the hostname, i.e. localhost.
 	define( 'JETPACK_DEV_DEBUG', true); // JETPACK DEVELOPMENT MODE
-	define('ENVIRONMENT', 'STAGING');
 } elseif ( stristr( $_SERVER['SERVER_NAME'], "domain.com" ) ) {
 	/* PRODUCTION SETTINGS */
 	define('DB_NAME', 'name');
 	define('DB_USER', 'user');
 	define('DB_PASSWORD', 'password');
 	define('DB_HOST', 'localhost');
-	define('DB_CHARSET', 'utf8');
-	define('DB_COLLATE', '');
-	define('WP_DEBUG', false);
+	define('WP_DEBUG', true);
+	define('WP_DEBUG_DISPLAY', false);
+	define('WP_DEBUG_LOG', true);
+	define('WP_MEMORY_LIMIT', '99M');
+	define('WP_MAX_MEMORY_LIMIT', '256M' );
 	define('DISALLOW_FILE_EDIT', true); // DISABLE WP FILE EDITOR
-	define('DOMAIN_CURRENT_SITE', 'domain.com');
 	define('SAVEQUERIES', false ); // USED WITH DEBUG BAR
 	define('WP_CACHE', true); // WP DEFAULT CACHING
-	define( 'LOCALINDICATOR_TEXT', 'LIVE'); // USED BY LOCAL INDICATOR
+	define('WPLT_SERVER', 'PRODUCTION'); // USED BY WP LOCAL TOOLBOX
+	//If not defined as PRODUCTION or LIVE, the plugin will enable
+	//Discourage search engines from indexing this site.
+	define('WPLT_COLOR', 'green'); // USED BY WP LOCAL TOOLBOX
 	// NOTE: Jetpack will automatically enable development mode if
 	// there isn't a period in the hostname, i.e. localhost.
 	define( 'JETPACK_DEV_DEBUG', false); // JETPACK DEVELOPMENT MODE
-	define('ENVIRONMENT', 'PRODUCTION');
 } else {
 	die('Please check wp-config.php file for correct SERVER_NAME settings.');
 }
+
+//SHARED CONFIG VALUES
+$table_prefix  = 'get_';
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('WPLANG', '');
 
 /* AUTHENTICATION UNIQUE KEYS AND SALTS. */
 /* https://api.wordpress.org/secret-key/1.1/salt/ */
@@ -69,17 +81,8 @@ define('SECURE_AUTH_SALT', 'generate your keys');
 define('LOGGED_IN_SALT',   'generate your keys');
 define('NONCE_SALT',       'generate your keys');
 
-$table_prefix  = 'wp_';
-define('WPLANG', '');
-
-/* CUSTOM SETTINGS */
-define('WP_MEMORY_LIMIT', '120M');
-define('WP_POST_REVISIONS', 15);
-define('ALTERNATE_WP_CRON', false);
-define('UB_HIDE_ADMIN_MENU', true); //HIDE WPMUDEV BRANDING
-define('PI_CRON_DEBUG', true); // ENABLE POST INDEXER CRON LOGGING.
-
-/* THAT'S ALL, STOP EDITING! HAPPY BLOGGING. */
+//INITIALIZE WP
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
+
 require_once(ABSPATH . 'wp-settings.php');
